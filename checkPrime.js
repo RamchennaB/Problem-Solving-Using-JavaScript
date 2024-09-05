ff5ca02b5e7bb4f1a1714bbdefd0be420b2633dc
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-09-05 09:09:39 +0530

    Update checkPrime.js

--- checkPrime.js
+++ checkPrime.js
@@ -3,22 +3,19 @@ class Solution {
     isPrime(n){
         
         //code here
-        let res=true;
-        if(n <=1){
-            res=false;
-        }
-        for(let i=2;i<n-1;i++){
-            if(n % i===0){
-                res=false;
-                break;
-            }
-        }
-        if(res){
-            return 1;
-        }
-        else{
+        if(n<=1){
             return 0;
         }
-        
-    }
+        if(n === 2){
+            return 1;
+        }
+        if(n % 2 === 0){
+            return 0;
+        }
+        for(let i=3; i*i<=n; i+=2){
+            if(n % i === 0){
+                return 0;
+            }
+        }
+        return 1;
 }
