8098217924be817c4b3bcd1731c2c06dde10480a
Ramchenna B <96220805+RamchennaB@users.noreply.github.com>
2024-09-23 19:55:55 +0530

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -15,48 +15,49 @@ class LinkedList{
         if(!this.head){
             this.head=newNode;
             this.tail=newNode;
-        }else{
+        }
+        else{
             newNode.next=this.head;
             this.head=newNode;
         }
     }
-   addAtEnd(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
-    } else {
-
-        this.tail.next = newNode;
-        this.tail = newNode; 
+    addAtPos(data, index){
+        const newNode=new Node(data);
+        if(index < 0){
+         console.log("Invalid position");
+        }
+        if(index < 1){
+            newNode.next=this.head;
+            this.head=newNode;
+        }
+        else{
+            let current =this.head;
+            for(let i=0;i<index - 1&& current!==null;i++){
+                current=current.next;
+            }
+            if(current==null){
+                console.log("invalid index");
+            }
+            else{
+                newNode.next=current.next;
+                current.next=newNode;
+            }
+        }
+        
     }
-  }
-  addAtPos(data,index){
-      const newNode=new Node(data);
-      if(index < 0){
-          console.log("invalid position");
-      }
-      if(index == 0){
-          newNode.next=this.head;
-          this.head=newNode;
-      }
-      else{
-          let current = this.head;
-          for(let i=0;i<index-1 && current!==null;i++){
-              current=current.next;
-          }
-          if(!current){
-              console.log("invalid position");
-          }
-          else{
-              newNode.next=current.next;
-              current.next=newNode;
-          }
-      }
-  }
-
+    addAtEnd(data){
+        const newNode=new Node(data);
+        if(!this.head){
+            this.head=newNode;
+            this.tail=newNode;
+        }else{
+            this.tail.next=newNode;
+            this.tail=newNode;
+        }
+    }
+    
     printAll(){
-        let current = this.head;
+        let current=this.head;
         while(current!==null){
             console.log(current.data);
             current=current.next;
@@ -66,6 +67,6 @@ class LinkedList{
 const ll=new LinkedList();
 ll.addAtBeg(1);
 ll.addAtBeg(2);
-ll.addAtEnd(3);
-ll.addAtPos(4,2);
+ll.addAtPos(3,1);
+ll.addAtEnd(4);
 ll.printAll();
